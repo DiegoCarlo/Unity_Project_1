e69ba9131e5e43b25b67922d52e00ca84e58e089
DiegoCarlo <diego.carlo.guerini@muhack.org>
2016-12-03 16:11:30 +0100

    Project Upload
    Fist code upload

---
+++ BouncingWorld_1 (1).0/Assets/Camera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset; // setta il valore dallo script se è privato. 
+	/* voglio che sia la differenza tra la distanza della camera e la distanza del player
+   */
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position; // Transform è la finestra che indica le posizioni
+	}
+
+	// Update is called once per frame, each frame we can track the position of the game object and set the position of
+	// the camera; but
+	// it's better to use LATEUPDATE, that runs every frame just like Update, but it's guaranteed to run after
+	// every item has been processed in Update. So when we set the position of the camera we know that the player
+	// has moved for that frame.
+
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ BouncingWorld_1 (1).0/Assets/SquareSheep.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquareSheep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){ //Codice chiamato ogni volta che il Player tocca un trigger collider (i cubi)
+		if (other.gameObject.CompareTag("Ground")) { //se l'oggetto con cui collide il player ha un tag "Ground":
+			// Per poter usare un tag, questo va prima aggiunto nell'elenco dei tag ed assegnato all'oggetto voluto
+			//other.gameObject.SetActive(false); // Questo fa sparire l'oggetto
+
+			print(gameObject.name);
+
+			GameObject newPec = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			newPec = gameObject; // 1) DARE AL NUOVO OGGETTO LE STESSE PROPRIETA' DEL PADRE
+			// 2) 
+			newPec.AddComponent<MeshCollider>();
+			//go.AddComponent<MyCustomScript>();
+		}
+	}
+}
